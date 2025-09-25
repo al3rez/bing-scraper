15ff47a28695d623326c4bba555276b7f980da55
Alireza Bashiri <al3rez@gmail.com>
2025-09-25 16:53:00 +0700

    fix: xss issue

--- app/serializers/dashboard_keyword_serializer.rb
+++ app/serializers/dashboard_keyword_serializer.rb
@@ -1,13 +1,17 @@
 class DashboardKeywordSerializer
   include JSONAPI::Serializer
 
-  attributes :phrase, :status, :ads_count, :links_count
+  attributes :status, :ads_count, :links_count
+
+  attribute :phrase do |keyword|
+    ActionController::Base.helpers.sanitize(keyword.phrase)
+  end
 
   attribute :scraped_at do |keyword|
     keyword.scraped_at&.iso8601
   end
 
   attribute :keyword_upload_original_filename do |keyword|
-    keyword.keyword_upload_original_filename
+    ActionController::Base.helpers.sanitize(keyword.keyword_upload_original_filename)
   end
 end
